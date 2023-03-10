b8f8245c1703ad38e9068c5c162a4b7e3b244e33
Radoslav <minecradi7@gmail.com>
2023-03-10 12:32:27 +0200

    updated main

--- Original(working)/main.cpp
+++ Original(working)/main.cpp
@@ -15,15 +15,22 @@ class User
         string email = "";
         string password;
         int age;
-        double averageSleep = 0, averagePulse = 0, averageWorkout = 0, averageSteps = 0;
+        double averageSleep = 0, averagePulse = 0, averageWorkout = 0, averageSteps = 0, averageWater = 0 ;
 
     private:
         int totalPulse = 0, pulseCounter = 0;
         int totalSleep = 0, sleepCounter = 0;
         int totalWorkout = 0, workoutCounter = 0;
         int totalSteps = 0, stepsCounter = 0;
-
+        int totalWater = 0, waterCounter = 0;
     public:
+         double addWater(int water)
+        {
+          this->totalWater+=water;
+          waterCounter++;
+          averageWater=totalWater/waterCounter;
+          return averageWater;
+        }
         double addSleep(int sleep)
         {
             this->totalSleep+=sleep;
@@ -41,7 +48,7 @@ class User
         double addWorkout(int workout)
         {
             this->totalWorkout+=workout;
-            sleepCounter++;
+            workoutCounter++;
             averageWorkout=totalWorkout/workoutCounter;
             return averageWorkout;
         }
@@ -109,7 +116,12 @@ void Register()
     string user_hash = Hash(email + password);
 
     users[user_hash] = user;
-    cout << "You have successfully registered";
+    cout << "You have successfully registered"<<endl;
+    cout << "Now you can login, logout or exit"<<endl;
+    cout << "List of commands:"<< endl;
+    cout << "login - logs into an account,"<<endl;
+    cout << "logout - logs out of the active account,"<<endl;
+    cout << "exit - exits the program."<<endl;
 }
 
 void LogIn()
@@ -133,7 +145,13 @@ void LogIn()
       // found
         active_user = users[user_hash];
         logged_in = true;
-        cout << "You have successfully logged in";
+        cout << "You have successfully logged in"<<endl;
+        cout << "New commands you can use: "<<endl;
+        cout << "addsleep - adds sleep,"<<endl;
+        cout << "addsteps - adds steps,"<<endl;
+        cout << "addpulse - adds pulse,"<<endl;
+        cout << "addwater - adds water,"<<endl;
+        cout << "addworkout - adds workout."<<endl;
     }
 }
 
@@ -141,6 +159,65 @@ void LogOut()
 {
     active_user = User();
     logged_in = false;
+    cout << "You have successfully logged out."<<endl;
+}
+void AddWorkout()
+{
+  if(active_user.email!="")
+  {
+    int workout;
+    double avg_workout;
+    cout << "How many workouts did you do?: ";
+    cin >> workout;
+    avg_workout=active_user.addWorkout(workout);
+    cout << "Your average workouts are: " << avg_workout << endl;
+    if(active_user.age < 5)
+    {
+     if(avg_workout >= 1)
+     {
+       cout << "Your average amount of workouts is good. \nKeep going!" << endl;
+     }
+    }
+     else if(active_user.age >= 5 || active_user.age <=12 && active_user.age >=60 )
+      {
+        if(avg_workout >=2)
+        {
+          cout << "Your average amount of workouts is good. \nKeep going!" << endl;
+        } else
+        {
+        cout << "Your average amount of workouts is bad. \nTry harder!" << endl;
+        }
+      }
+    else if (active_user.age > 12 || active_user.age < 60)
+    {
+      if(avg_workout >=3)
+    {
+      cout << "Your average amount of workouts is good. \nKeep going!" << endl;
+    } else
+    {
+      cout << "Your average amount of workouts is bad. \nTry harder!" << endl;
+    }
+      }
+  }
+}
+void AddWater()
+{
+  if(active_user.email!="")
+  {
+    int water;
+    double avg_water;
+    cout << "How much water you consumed in liters?: ";
+    cin >> water;
+    avg_water=active_user.addWater(water);
+    cout << "Your average consumed water is: " << avg_water << endl;
+    if (avg_water >= 2)
+    {
+      cout << "Your consumed water is enough. \nWell done!" << endl;
+    } else
+    {
+       cout << "Your consumed water isn't enough. \nDrink more water!" << endl;
+    }
+  }
 }
 
 void AddSleep()
@@ -158,13 +235,13 @@ void AddSleep()
         if (avg_sleep >= 8) {
           cout << "Your average amount of sleep is good." << endl;
         } else {
-          cout << "Your average amount of sleep is bad." << endl;
+          cout << "Your average amount of sleep is bad.\nSleep more. Feel better!" << endl;
         }
       } else {
         if (avg_sleep >= 7) {
           cout << "Your average amount of sleep is good." << endl;
         } else {
-          cout << "Your average amount of sleep is bad." << endl;
+          cout << "Your average amount of sleep is bad.\nSleep more. Feel better!" << endl;
         }
       }
     }
@@ -183,42 +260,42 @@ void addPulse()
     case 1:;
     case 2:
         {
-            if(averagePulse>130) cout<<"\nPulse is too high";
-            else if(averagePulse<80) cout<<"\nPulse is too low";
-            else cout<<"\nPulse is within acceptable range";
+            if(averagePulse>130) cout<<"\nPulse is too high. \nBe calmer!";
+            else if(averagePulse<80) cout<<"\nPulse is too low. \nBe more active!";
+            else cout<<"\nPulse is within acceptable range.";
             break;
         }
     case 3:;
     case 4:
         {
-            if(averagePulse>120) cout<<"\nPulse is too high";
-            else if(averagePulse<80) cout<<"\nPulse is too low";
-            else cout<<"\nPulse is within acceptable range";
+            if(averagePulse>120) cout<<"\nPulse is too high. \nBe calmer!";
+            else if(averagePulse<80) cout<<"\nPulse is too low. \nBe more active!";
+            else cout<<"\nPulse is within acceptable range.";
             break;
         }
     case 5:;
     case 6:
         {
-            if(averagePulse>115) cout<<"\nPulse is too high";
-            else if(averagePulse<75) cout<<"\nPulse is too low";
-            else cout<<"\nPulse is within acceptable range";
+            if(averagePulse>115) cout<<"\nPulse is too high. \nBe calmer!";
+            else if(averagePulse<75) cout<<"\nPulse is too low. \nBe more active!";
+            else cout<<"\nPulse is within acceptable range.";
             break;
         }
     case 7:;
     case 8:;
     case 9:
         {
-            if(averagePulse>110) cout<<"\nPulse is too high";
-            else if(averagePulse<70) cout<<"\nPulse is too low";
-            else cout<<"\nPulse is within acceptable range";
+            if(averagePulse>110) cout<<"\nPulse is too high. \nBe calmer!";
+            else if(averagePulse<70) cout<<"\nPulse is too low. \nBe more active!";
+            else cout<<"\nPulse is within acceptable range.";
 
             break;
         }
     default :
         {
-            if(averagePulse>100) cout<<"\nPulse is too high";
-            else if(averagePulse<60) cout<<"\nPulse is too low";
-            else cout<<"\nPulse is within acceptable range";
+            if(averagePulse>100) cout<<"\nPulse is too high. \nBe calmer!";
+            else if(averagePulse<60) cout<<"\nPulse is too low. \nBe more active!";
+            else cout<<"\nPulse is within acceptable range.";
         }
 
     }
@@ -235,37 +312,37 @@ void addSteps(){
         cout << "Your average steps are: " << averageSteps << endl;
         if (active_user.age < 5) {
             if (averageSteps >= 2000) {
-              cout << "Your average amount of steps is good." << endl;
+              cout << "Your average amount of steps is good. " << endl;
             } else {
-              cout << "Your average amount of steps is not enough." << endl;
+              cout << "Your average amount of steps is not enough. Keep walking!" << endl;
             }
         }
         else if (active_user.age >5 && active_user.age <=13) {
             if (averageSteps >= 8000) {
               cout << "Your average amount of steps is good." << endl;
             } else {
-              cout << "Your average amount of steps is not enough." << endl;
+              cout << "Your average amount of steps is not enough. Keep walking!" << endl;
             }
        }
        else if (active_user.age >13 && active_user.age <=45) {
             if (averageSteps >= 10000) {
               cout << "Your average amount of steps is good." << endl;
             } else {
-              cout << "Your average amount of steps is not enough." << endl;
+              cout << "Your average amount of steps is not enough. Keep walking!" << endl;
             }
        }
        else if (active_user.age >45 && active_user.age <=60) {
             if (averageSteps >= 8000) {
               cout << "Your average amount of steps is good." << endl;
             } else {
-              cout << "Your average amount of steps is not enough." << endl;
+              cout << "Your average amount of steps is not enough. Keep walking!" << endl;
             }
        }
        else if (active_user.age >60) {
             if (averageSteps >= 6000) {
               cout << "Your average amount of steps is good." << endl;
             } else {
-              cout << "Your average amount of steps is not enough." << endl;
+              cout << "Your average amount of steps is not enough. Keep walking" << endl;
             }
        }
      }
@@ -275,11 +352,10 @@ void addSteps(){
 int main()
 {
     string input;
-    cout << "List of commands: \n"; //to finish
-    cout << "exit - exits the program\n";
-    cout << "login - logs in an account\n";
-    cout << "logout - log out of the active account\n";
-    cout << "register - register\n";
+    cout << "First you have to register or exit." << endl;
+    cout << "List of commands:"<< endl;
+    cout << "register - register,"<<endl;
+    cout << "exit - exits the program."<<endl;
     cin >> input;
 
     for(int i=0; i < input.length();i++)
@@ -302,6 +378,15 @@ int main()
         }
         else if(input=="addsleep"){
             AddSleep();
+        }
+         else if(input=="addworkout"){
+            AddWorkout();
+        }
+        else if(input=="addwater"){
+            AddWater();
+        }
+        else if(input=="addsteps"){
+            addSteps();
         }
         cout << "\n";
         cin >> input;
